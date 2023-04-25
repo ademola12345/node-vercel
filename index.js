d33b2f0caffbedbed3f76e27bb3d7a7b121a7892
ademola12345 <87110640+ademola12345@users.noreply.github.com>
2023-04-25 06:24:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
